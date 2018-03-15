b7239605e5aa2abd108ebfb10fcf55388a356ea3
cktang88 <arctangent20@yahoo.com>
2018-03-15 02:29:08 -0500

    Init

---
+++ quotes.js
@@ -0,0 +1,215 @@
+
+window.quotes = `*Thousands of candles can be lighted from a single candle, and the life of the candle will not be shortened. Happiness never decreases by being shared.*
+Buddha
+*Happiness is the art of never holding in your mind the memory of any unpleasant thing that has passed.*
+Unknown
+*To be happy, we must not be too concerned with others.*
+Albert Camus
+*If you want happiness for an hour — take a nap.
+If you want happiness for a day — go fishing.
+If you want happiness for a year — inherit a fortune.
+If you want happiness for a lifetime — help someone else.*
+Chinese Proverb
+*The moments of happiness we enjoy take us by surprise. It is not that we seize them, but that they seize us.*
+Ashley Montagu
+*Don’t rely on someone else for your happiness and self-worth. Only you can be responsible for that. If you can’t love and respect yourself – no one else will be able to make that happen. Accept who you are – completely; the good and the bad – and make changes as YOU see fit – not because you think someone else wants you to be different.*
+Stacey Charter
+*It isn’t what you have, or who you are, or where you are, or what you are doing that makes you happy or unhappy. It is what you think about.*
+Dale Carnegie
+*It’s a helluva start, being able to recognize what makes you happy.*
+Lucille Ball
+*Don’t underestimate the value of Doing Nothing, of just going along, listening to all the things you can’t hear, and not bothering.*
+Winnie the Pooh
+*There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will.*
+Epictetus
+*We tend to forget that happiness doesn’t come as a result of getting something we don’t have, but rather of recognizing and appreciating what we do have.*
+Frederick Keonig
+*Sometimes your joy is the source of your smile, but sometimes your smile can be the source of your joy.*
+Thich Nhat Hanh
+*Perhaps they are not stars, but rather openings in heaven where the love of our lost ones pours through and shines down upon us to let us know they are happy.*
+Eskimo Proverb
+*To be kind to all, to like many and love a few, to be needed and wanted by those we love, is certainly the nearest we can come to happiness.*
+Mary Stuart
+*There are more things to alarm us than to harm us, and we suffer more often in apprehension than reality.*
+Seneca
+*Love is that condition in which the happiness of another person is essential to your own.*
+Robert A. Heinlein
+*Happy people plan actions, they don’t plan results.*
+Dennis Waitley
+*Happiness is when what you think, what you say, and what you do are in harmony.*
+Mahatma Gandhi
+*The only joy in the world is to begin.*
+Cesare Pavese
+*Some cause happiness wherever they go; others whenever they go*
+Oscar Wilde
+*Time you enjoy wasting is not wasted time.*
+Marthe Troly-Curtin
+*Nobody can be uncheered with a balloon*
+Winnie the Pooh
+*Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.*
+Herman Cain
+*What you do not want done to yourself, do not do to others.*
+Confucius
+*There is only one cause of unhappiness: the false beliefs you have in your head, beliefs so widespread, so commonly held, that it never occurs to you to question them.*
+Anthony de Mello
+*Happiness is not something ready-made. It comes from your own actions.*
+Dalai Lama
+*When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.*
+Helen Keller
+*Happiness depends upon ourselves.*
+Aristotle
+*It is more fitting for a man to laugh at life than to lament over it.*
+Seneca
+*The reason people find it so hard to be happy is that they always see the past better than it was, the present worse than it is, and the future less resolved than it will be.*
+Marcel Pagnol
+*If men would consider not so much wherein they differ, as wherein they agree, there would be far less of uncharitableness and angry feeling in the world.*
+Joseph Addison
+*Happiness is having a large, loving, caring, close-knit family in another city.*
+George Burns
+*Happiness is not in the mere possession of money; it lies in the joy of achievement, in the thrill of creative effort.*
+Franklin D. Roosevelt
+*The pleasure which we most rarely experience gives us greatest delight.*
+Epictetus
+*It’s been my experience that you can nearly always enjoy things if you make up your mind firmly that you will.*
+L.M. Montgomery
+*Happiness is acceptance.*
+Unknown
+*The secret of happiness is not in doing what one likes, but in liking what one does.*
+James M. Barrie
+*We begin from the recognition that all beings cherish happiness and do not want suffering. It then becomes both morally wrong and pragmatically unwise to pursue only one’s own happiness oblivious to the feelings and aspirations of all others who surround us as members of the same human family. The wiser course is to think of others when pursuing our own happiness.*
+Dalai Lama
+*Most people would rather be certain they’re miserable, than risk being happy.*
+Dr. Robert Anthony
+*The unhappy derive comfort from the misfortunes of others.*
+Aesop
+*For many men, the acquisition of wealth does not end their troubles, it only changes them.*
+Seneca
+*A table, a chair, a bowl of fruit and a violin; what else does a man need to be happy?*
+Albert Einstein
+*Of all forms of caution, caution in love is perhaps the most fatal to true happiness.*
+Bertrand Russell
+*The most beautiful people we have known are those who have known defeat, known suffering, known struggle, known loss, and have found their way out of the depths. These persons have an appreciation, a sensitivity and an understanding of life that fills them with compassions, gentleness, and a deep loving concern. Beautiful people do not just happen.*
+Elizabeth Kubler-Ross
+*Happiness is a myth we seek,
+If manifested surely irks;
+Like river speeding to the plain,
+On its arrival slows and murks.
+For man is happy only in
+His aspiration to the heights;
+When he attains his goal, he cools
+And longs for other distant flights.*
+Kahlil Gibran
+*Happiness is a state of activity.*
+Aristotle
+*This planet has – or rather had – a problem, which was this: most of the people living on it were unhappy for pretty much of the time. Many solutions were suggested for this problem, but most of these were largely concerned with the movement of small green pieces of paper, which was odd because on the whole it wasn’t the small green pieces of paper that were unhappy.*
+Douglas Adams
+*Since you get more joy out of giving joy to others, you should put a good deal of thought into the happiness that you are able to give.*
+Eleanor Roosevelt
+*Life is really simple, but we insist on making it complicated.*
+Confucius
+*The two enemies of human happiness are pain and boredom.*
+Arthur Schopenhauer
+*Men spend their lives in anticipations, in determining to be vastly happy at some period when they have time. But the present time has one advantage over every other – it is our own. Past opportunities are gone, future have not come. We may lay in a stock of pleasures, as we would lay in a stock of wine; but if we defer the tasting of them too long, we shall find that both are soured by age.*
+Charles Caleb Colton
+*Very little is needed to make a happy life; it is all within yourself, in your way of thinking.*
+Marcus Aurelius Antoninus
+*Happy he who learns to bear what he cannot change.*
+Friedrich Schiller
+*When I look back on all these worries, I remember the story of the old man who said on his deathbed that he had had a lot of trouble in his life, most of which had never happened.*
+Winston Churchill
+*I’d far rather be happy than right any day.*
+Douglas Adams
+*Everyone wants to live on top of the mountain, but all the happiness and growth occurs while you’re climbing it.*
+Andy Rooney
+*The foolish man seeks happiness in the distance, the wise grows it under his feet.*
+James Oppenheim
+*Action may not always bring happiness; but there is no happiness without action.*
+Benjamin Disraeli
+*The greater part of our happiness or misery depends upon our dispositions, and not upon our circumstances.*
+Martha Washington
+*Happiness is nothing more than good health and a bad memory.*
+Albert Schweitzer
+*Our envy always lasts longer than the happiness of those we envy.*
+Heraclitus
+*Happiness is a how; not a what. A talent, not an object.*
+Herman Hesse
+*No act of kindness, no matter how small, is ever wasted.*
+Aesop
+*Every man has his secret sorrows which the world knows not; and often times we call a man cold when he is only sad.*
+Henry Wadsworth Longfellow
+*Happiness is something that comes into our lives through doors we don’t even remember leaving open.*
+Rose Lane
+*The best years of your life are the ones in which you decide your problems are your own. You do not blame them on your mother, the ecology, or the president. You realize that you control your own destiny.*
+Albert Ellis
+*I, not events, have the power to make me happy or unhappy today. I can choose which it shall be. Yesterday is dead, tomorrow hasn’t arrived yet. I have just one day, today, and I’m going to be happy in it.*
+Groucho Marx
+*Just because it didn’t last forever, doesn’t mean it wasn’t worth your while.*
+Unknown
+*Your work is discover your world and then with all your heart give yourself to it.*
+Buddha
+*That man is richest whose pleasures are cheapest.*
+Henry David Thoreau
+*Happiness always looks small while you hold it in your hands, but let it go, and you learn at once how big and precious it is.*
+Maxim Gorky
+*A quiet secluded life in the country, with the possibility of being useful to people to whom it is easy to do good, and who are not accustomed to have it done to them; then work which one hopes may be of some use; then rest, nature, books, music, love for one’s neighbor — such is my idea of happiness.*
+Leo Tolstoy
+*It was only a sunny smile, and little it cost in the giving, but like morning light it scattered the night and made the day worth living.*
+F. Scott Fitzgerald
+*If thou wilt make a man happy, add not unto his riches but take away from his desires.*
+Epicurus
+*Plenty of people miss their share of happiness, not because they never found it, but because they didn’t stop to enjoy it.*
+William Feather
+*Gratitude is a vaccine, an antitoxin, and an antiseptic.*
+John Henry Jowett
+*No one can make you feel inferior without your consent.*
+Eleanor Roosevelt
+*And remember, no matter where you go, there you are.*
+Confucius
+*If you are too busy to laugh, you are too busy.*
+Proverb
+*Security is mostly a superstition. It does not exist in nature…. Life is either a daring adventure or nothing.*
+Helen Keller
+*For most of life, nothing wonderful happens. If you don’t enjoy getting up and working and finishing your work and sitting down to a meal with family or friends, then the chances are you’re not going to be very happy. If someone bases his/her happiness on major events like a great job, huge amounts of money, a flawlessly happy marriage or a trip to Paris, that person isn’t going to be happy much of the time.
+If, on the other hand, happiness depends on a good breakfast, flowers in the yard, a drink or a nap, then we are more likely to live with quite a bit of happiness.*
+Andy Rooney
+*Learn to let go. That is the key to happiness.*
+Buddha
+*The first recipe for happiness is: avoid too lengthy meditation on the past.*
+Andre Maurois
+*The grass is always greener where you water it.*
+Unknown
+*Enjoy your own life without comparing it with that of another.*
+Marquis de Condorcet
+*On a deeper level you are already complete. When you realize that, there is a playful, joyous energy behind what you do.*
+Eckhart Tolle
+*The happiest people in the world are those who feel absolutely terrific about themselves, and this is the natural outgrowth of accepting total responsibility for every part of their life.*
+Brian Tracy
+*Let us be grateful to the people who make us happy; they are the charming gardeners who make our souls blossom.*
+Marcel Proust
+*We don’t stop playing because we grow old; we grow old because we stop playing.*
+George Bernard Shaw
+*Worry never robs tomorrow of its sorrow. It only saps today of its joy.*
+Leo Buscaglia
+*A well-developed sense of humor is the pole that adds balance to your steps as you walk the tightrope of life.*
+William Arthur Ward
+*Optimism is a happiness magnet. If you stay positive, good things and good people will be drawn to you.*
+Mary Lou Retton
+*I believe compassion to be one of the few things we can practice that will bring immediate and long-term happiness to our lives.*
+Dalai Lama
+*Follow your bliss and don’t be afraid, and doors will open where you didn’t know they were going to be.*
+Joseph Campbell
+*Happiness consists of living each day as if it were the first day of your honeymoon and the last day of your vacation.*
+Leo Tolstoy
+*Most people are about as happy as they make up their minds to be.*
+Abraham Lincoln
+*Being happy doesn’t mean everything is perfect. It means you’ve decided to look beyond the imperfections.*
+Unknown
+*The deeper that sorrow carves into your being, the more joy you can contain. Is not the cup that holds your wine the very cup that was burned in the potter’s oven? And is not the lute that soothes your spirit, the very wood that was hollowed with knives?
+When you are joyous, look deep into your heart and you shall find it is only that which has given you sorrow that is giving you joy. When you are sorrowful look again in your heart, and you shall see in truth that you are weeping for that which has been your delight.*
+Kahlil Gibran
+*If you spend your whole life waiting for the storm, you’ll never enjoy the sunshine.*
+Morris West
+*Life will bring you pain all by itself. Your responsibility is to create joy.*
+Milton Erickson
+*Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.*
+Mark Twain`
